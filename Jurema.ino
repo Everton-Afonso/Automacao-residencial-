157f5c46a993baa5d492ea52a9ad0d991b08b31e
Everton Otavio Afonso <evertonafonso0@gmail.com>
2020-03-23 07:42:31 -0300

    add Jurema.ino

---
+++ Jurema.ino
@@ -0,0 +1,21 @@
+int porta_rele2 = 7;
+int state = 0;
+
+void setup() {
+  pinMode(porta_rele2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    state = Serial.read();
+ }
+ if (state == '0') {
+  digitalWrite(porta_rele2, HIGH); 
+  state = 0;
+ }
+ else if (state == '1') {
+  digitalWrite(porta_rele2, LOW);
+  state = 0;
+ } 
+}
